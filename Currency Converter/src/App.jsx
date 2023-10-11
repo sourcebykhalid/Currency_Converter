a877d6a7e3f123b8a09ac4f4693e12ac671fade9
Khalid Shakeel <khalidshakeel46@gmail.com>
2023-10-11 12:23:34 +0530

    Currency Converter using React

---
+++ Currency Converter/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="flex justify-center bg-slate-400 text-4xl rounded-l shadow-2xl text-black font-extrabold p-2 m-24">
+        Currency Converter
+      </h1>
+      CurrencyConverter
+    </>
+  );
+}
+
+export default App;
